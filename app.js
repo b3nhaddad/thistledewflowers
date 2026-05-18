86b0e873d88fee2cb1db6c19a16e92eb78ffe3af
Benjamin Haddad <couchroomkid@Benjamins-MacBook-Air-109.local>
2026-05-18 16:30:10 -0600

    initial

---
+++ app.js
@@ -0,0 +1,16 @@
+const observer = new IntersectionObserver((entries) => {
+entries.forEach((entry) =>{
+	console.log(entry)
+	if(entry.isIntersecting && entry.target.classList.contains('left-scroll')){
+    entry.target.classList.add('showleft');
+}
+	if(entry.isIntersecting){
+	entry.target.classList.add('show');
+}
+
+});
+});
+
+const hiddenElements = document.querySelectorAll('.hidden, .left-scroll');
+
+hiddenElements.forEach((el) => observer.observe(el));
